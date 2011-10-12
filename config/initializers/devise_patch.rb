d631dc539416a50ab307d70f5933ad8f02dbcc1c
gerald <gerald@gerald-desktop>
2011-10-12 12:36:12 +0100

    make the patch more generic

--- config/initializers/devise_patch.rb
+++ config/initializers/devise_patch.rb
@@ -1,3 +1,3 @@
 Devise::PasswordsController.send(:define_method, :after_sending_reset_password_instructions_path_for) do |resource_name|
-  new_user_session_path
+  "new_#{resource_name}_session".to_sym
 end
